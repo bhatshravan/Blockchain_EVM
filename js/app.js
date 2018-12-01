4273c91d290763b7317ffe7e99090a85d9144c4e
shravan <shrvnbhat0@gmail.com>
2018-12-01 11:45:20 +0530

    Added all files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var animationEnd = (function(el) {
+    var animations = {
+      animation: 'animationend',
+      OAnimation: 'oAnimationEnd',
+      MozAnimation: 'mozAnimationEnd',
+      WebkitAnimation: 'webkitAnimationEnd',
+    };
+  
+    for (var t in animations) {
+      if (el.style[t] !== undefined) {
+        return animations[t];
+      }
+    }
+  })(document.createElement('div'));
+  
+  $('#header').one(animationEnd, function(){
+    $("#tableDiv").removeClass('hidden');
+  });
---
+++ js/done.js
@@ -0,0 +1,9 @@
+function closeapp() {
+//  var url = new URL(location.href);
+//  var uid = url.searchParams.get("uid");
+/*
+  console.log("prtest");
+  console.log(uid);
+  Android.showToast("Yo mama");
+  */Android.finishVoting("test");
+}
